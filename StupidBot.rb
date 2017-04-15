7cd8083bb5f009b79991f1b4e1c8c3cac4fdcbf4
Xavier <xtheman166@gmail.com>
2017-04-15 12:54:57 -0400

    Update StupidBot.rb

--- StupidBot.rb
+++ StupidBot.rb
@@ -19,3 +19,7 @@ bot.command(:game, description: 'what do you think') do |event, *game|
             event.bot.game = game.join(' ')
             event.respond("Game set to `#{game.join(' ')}`")
 end
+
+bot.command(:pm) do |event|
+  event.user.pm ('Fuck you bitch')
+end
